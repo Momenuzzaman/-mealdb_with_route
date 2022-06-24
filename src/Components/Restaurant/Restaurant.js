a3d90d7f301afc904e2c08cff35286c8dd882b5e
mdemon420 <mdeamon4@gmail.com>
2022-06-25 00:43:44 +0600

    load meal data

--- src/Components/Restaurant/Restaurant.js
+++ src/Components/Restaurant/Restaurant.js
@@ -1,16 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Restaurant = () => {
-    const [conText,setConText] = useState('');
+    const [searchText,setSearchText] = useState('');
+    const [meals,setMeals] = useState([]);
+    
+    useEffect(() => {
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`)
+        .then(response => response.json())
+        .then(data => setMeals(data.meals))
+    },[searchText])
 
     const handleSearch = (event) => {
         const searchValue = event.target.value;
-        console.log(searchValue);
+        setSearchText(searchValue);
     }
 
     return (
         <div>
             <input onChange={handleSearch} type="text" placeholder="Search Food"></input>
+            
         </div>
     );
 };
