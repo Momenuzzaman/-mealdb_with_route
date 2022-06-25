a4b192979f145d5c4a61fcebc10924d9cbb63785
mdemon420 <mdeamon4@gmail.com>
2022-06-25 23:19:51 +0600

    pass data

--- src/Components/MealDetail/MealDetail.js
+++ src/Components/MealDetail/MealDetail.js
@@ -1,12 +1,23 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Detail from "../detail/Detail";
 
 
 
 const MealDetail = () => {
     const {mealId} = useParams()
+    const [mealDetail,setDetail] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+        .then(response => response.json())
+        .then(data => setDetail(data.meals))
+    },[]);
     return (
         <div>
-            <h1>{mealId}</h1>
+            {
+            mealDetail.map(meal => <Detail meal = {meal}></Detail>)
+            }
         </div>
     );
 };
---
+++ src/Components/detail/Detail.js
@@ -0,0 +1,11 @@
+
+const Detail = (props) => {
+    const {strMeal} = props.meal;
+    return (
+        <div>
+            <h1>{strMeal}</h1>
+        </div>
+    );
+};
+
+export default Detail;
